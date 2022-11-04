2883e996d68bf75f1010a48d0c2cc5498798f3e3
lollobianco <loreb2002@gmail.com>
2022-11-04 15:13:47 +0100

    Aggiunte card "compilate" dinamicamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+const cardsContainer = document.getElementById('cardsContainer')
+
+const imgPath = './assets/img/'
+
 const team = [
    {
      name: 'Wayne Barnett',
@@ -29,4 +33,22 @@ const team = [
      role: 'Graphic Designer',
      image: 'barbara-ramos-graphic-designer.jpg',
    },
- ];
+];
+
+for (let i = 0; i < team.length; i++){
+
+   let element = team[i];
+   console.log(element.name, element.role);
+
+   cardsContainer.innerHTML += 
+   `
+   <div class="card col-3 m-3 p-0">
+      <img src=${imgPath + element.image} class="card-img-top" alt="profile-image">
+      <div class="card-body text-center">
+         <h5 class="card-title">${element.name}</h5>
+         <p class="card-text">${element.role}</p>
+      </div>
+   </div>
+   `
+
+}
